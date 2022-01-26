d866c1966a449d67b1ec0aa169fd920045d0e4e6
Harri Liimann <harri.liimann@gmail.com>
2022-01-27 00:26:30 +0200

    Fixed indentation

--- modules/play-music.js
+++ modules/play-music.js
@@ -1,17 +1,17 @@
 const { joinVoiceChannel, createAudioPlayer, createAudioResource, entersState, StreamType, AudioPlayerStatus, VoiceConnectionStatus, NoSubscriberBehavior} = require('@discordjs/voice');
 const player = createAudioPlayer({
    behaviors: {
-		noSubscriber: NoSubscriberBehavior.Pause,
-	},
+      noSubscriber: NoSubscriberBehavior.Pause,
+   },
 });
 
 async function startPlaying(channel) {
    const resource = createAudioResource("./mixes_temp/mix.mp3");
 
-	player.play(resource);
+   player.play(resource);
 
-	try {
-		await entersState(player, AudioPlayerStatus.Playing, 5_000);
+   try {
+      await entersState(player, AudioPlayerStatus.Playing, 5_000);
       // The player has entered the Playing state within 5 seconds
       const connection = joinVoiceChannel({
          channelId: channel.id,
@@ -22,13 +22,13 @@ async function startPlaying(channel) {
       connection.subscribe(player);
       console.log(player.state)
 
-		console.log('Playback has started!');
-	} catch (error) {
-		// The player has not entered the Playing state and either:
-		// 1) The 'error' event has been emitted and should be handled
-		// 2) 5 seconds have passed
-		console.error(error);
-	}
+      console.log('Playback has started!');
+   } catch (error) {
+      // The player has not entered the Playing state and either:
+      // 1) The 'error' event has been emitted and should be handled
+      // 2) 5 seconds have passed
+      console.error(error);
+   }
 }
 
 function stopPlaying() {
