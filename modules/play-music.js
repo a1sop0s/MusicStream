87c5997e030349f30b18ee4d01f1dcf45676ca24
Harri Liimann <harri.liimann@gmail.com>
2022-01-27 18:07:42 +0200

    Remove unPause func, transfer handling to play mdl

--- modules/play-music.js
+++ modules/play-music.js
@@ -1,4 +1,5 @@
 const { joinVoiceChannel, createAudioPlayer, createAudioResource, entersState, StreamType, AudioPlayerStatus, VoiceConnectionStatus, NoSubscriberBehavior} = require('@discordjs/voice');
+const { deleteMix } = require('./download-mix');
 const player = createAudioPlayer({
    behaviors: {
       noSubscriber: NoSubscriberBehavior.Pause,
@@ -20,21 +21,36 @@ async function startPlaying(channel) {
       });
       if (!connection) return console.error('fuck');
       connection.subscribe(player);
-      console.log(player.state)
 
       console.log('Playback has started!');
    } catch (error) {
       // The player has not entered the Playing state and either:
       // 1) The 'error' event has been emitted and should be handled
       // 2) 5 seconds have passed
-      console.error(error);
+      return console.error(error);
    }
 }
 
 function stopPlaying() {
-   player.stop();
+   if (player.state.status !== 'idle') {
+      player.stop();
+      deleteMix();
+   }
+}
+
+function pausePlaying() {
+   if (player.state.status !== 'paused' && player.state.status !== 'playing') return null;
+
+   if (player.state.status !== 'paused') {
+      player.pause();
+      return 'paused'
+   } else {
+      player.unpause();
+      return 'playing';
+   }
 }
 
 
 exports.startPlaying = startPlaying;
 exports.stopPlaying = stopPlaying;
+exports.pausePlaying = pausePlaying;
