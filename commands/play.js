d8b2e8d2abc5e268fae9a6ca618e94a77c18b0f7
Harri Liimann <harri.liimann@gmail.com>
2022-02-22 21:25:39 +0200

    Implemented pagination into scrape module

--- commands/play.js
+++ commands/play.js
@@ -1,6 +1,6 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageActionRow, MessageSelectMenu } = require('discord.js');
-const { retrieveStreams, retrieveShowlist } = require('../modules/scrape_r2.js');
+const { retrieveStreams } = require('../modules/scrape_r2.js');
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -12,6 +12,7 @@ module.exports = {
             .setRequired(true)
             .addChoice('Citylights FM', 'Citylights FM')
             .addChoice('Grindtape', 'Grindtape')
+            .addChoice('Programm', 'Programm')
             .addChoice('Tjuun In', 'Tjuun In')
             .addChoice('Urbanism', 'Urbanism')
          ),
@@ -19,17 +20,17 @@ module.exports = {
 
       async execute(interaction) {
          const selectedShow = await interaction.options.getString('show');
-         
+
          const streams = await retrieveStreams(selectedShow);
-         if (!streams) return interaction.reply('Something went wrong!');
+         if (!streams) return interaction.reply({ content: "Something went wrong!", ephemeral: true });
          const row = new MessageActionRow()
             .addComponents(
                new MessageSelectMenu()
                .setCustomId('podcastselect')
                .setPlaceholder('Nothing selected')
-               .addOptions(streams)
+               .addOptions(streams[0])
             );
 
-         await interaction.reply({ content: "Please select a mix or podcast:", components: [row] });
+         await interaction.reply({ content: "Please select a mix or podcast:", components: [row], ephemeral: true });
       }
 };
--- modules/scrape_r2.js
+++ modules/scrape_r2.js
@@ -5,65 +5,59 @@ const shows = require('../podcasts.json');
 
 async function retrieveStreams(show) {
    const _show = shows.podcasts.find(s => s.name === show);
+
+   // If for some reason the show cannot be found, return
    if (!_show) return;
 
+   // Array to be filled with paginated streams
    let showInfo = [];
 
+   // Retrieve and load the selected show's XML page
    const response = await axios(_show);
    const $ = cheerio.load(response.data, {
       xmlMode: true
    });
 
+   // Define items as all the elements that are named "item"
    const items = $('item');
-
    const arr = items.toArray();
-   
-   for (let i = 0; i < (arr.length < 25 ? arr.length : 25); i++) {
-      // If the podcast has less than 25 shows, limit iterations to the amount of shows (length of array)
+
+   // Pagination variables
+   let itemCount = 0;
+   let pageElements = [];
+
+   // Iterate through the shows and paginate
+   for (let i = 0; i < arr.length; i++) {
+
+      // Place all the page contents into an array
       const item = $(arr[i]);
+      // Locate the title, enclosure URL and publishing date of the podcast
       const _title = item.find('title').text();
       const _url = item.find('enclosure').attr('url');
       const _date = item.find('pubDate').text().substring(0, 16);
 
+      // Ignore if for some reason a podcast is missing any of the required fields
       if (!_title || !_date || !_url) return console.log(_title, _date, _url);
 
+      // Construct object with the fields above
       const infoObj = new Object();
       infoObj.label = _title;
       infoObj.description = _date;
       infoObj.value = _url;
 
-      showInfo.push(infoObj);
+      // Push infoObj into pageElements array and increase current itemCount
+      pageElements.push(infoObj);
+      itemCount++;
+
+      if (itemCount >= 25 || itemCount >= arr.length) {
+         // Reset itemCount, pageElements and push pageElements into showInfo array
+         itemCount = 0;
+         showInfo.push(pageElements);
+         pageElements = [];
+      }
    }
-   console.log(showInfo);
+   console.log(showInfo[0], showInfo[0].length);
    return showInfo;
 
 }
-
-// Returns the shows in a template that is ready to be fed into MessageSelectMenu
-async function retrieveShowlist(ignoreDesc) {
-   const returnShows = [];
-
-   if (ignoreDesc) {
-      shows.forEach(s => {
-         const minObj = new Object();
-         minObj.name = s.name;
-         minObj.value = s.name;
-
-         returnShows.push(minObj);
-      });
-   } else {
-      shows.forEach(s => {
-         const showObj = new Object();
-         showObj.label = s.name;
-        // showObj.description = s.desc;
-         showObj.value = s.name;
-
-         returnShows.push(showObj);
-      });
-   }
-   return returnShows;
-}
 exports.retrieveStreams = retrieveStreams;
-exports.retrieveShowlist = retrieveShowlist;
-//retrieveShowlist(true).then(sl => console.log(sl));
-//retrieveStreams("Tjuun In")
