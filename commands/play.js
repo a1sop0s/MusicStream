0cbd5c5fe20224c0b79479175ee5655986b5c0ec
Harri L <harri.liimann@gmail.com>
2022-03-01 21:01:31 +0200

    Implemented pagination, moved rows to handler

--- commands/play.js
+++ commands/play.js
@@ -1,6 +1,7 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageActionRow, MessageSelectMenu, Message, MessageButton } = require('discord.js');
 const { retrieveStreams } = require('../modules/scrape_r2.js');
+const { setStream } = require('../modules/handle-pagination');
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -23,26 +24,8 @@ module.exports = {
 
          const streams = await retrieveStreams(selectedShow);
          if (!streams) return interaction.reply({ content: "Something went wrong!", ephemeral: true });
-         const row = new MessageActionRow()
-            .addComponents(
-               new MessageSelectMenu()
-                  .setCustomId('podcastselect')
-                  .setPlaceholder('Nothing selected')
-                  .addOptions(streams[0]),
-            );
-         const buttonrow = new MessageActionRow()
-               .addComponents(
-                  new MessageButton()
-                     .setCustomId('podcasts_back')
-                     .setEmoji('945771799692599316')
-                     .setStyle('SECONDARY')
-                     .setDisabled(true),
-                  new MessageButton()
-                     .setCustomId('podcasts_next')
-                     .setEmoji('945771799789060096')
-                     .setStyle('PRIMARY')
-               )
+         const rows = setStream(streams);
 
-         await interaction.reply({ content: "Please select a mix or podcast:", components: [row, buttonrow], ephemeral: true });
+         await interaction.reply({ content: `Please select a mix or podcast: (Page 1/${streams.length})`, components: [rows[0], rows[1]], ephemeral: true });
       }
 };
--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,25 +1,29 @@
 const { startPlaying, stopPlaying } = require('../modules/play-music');
+const { handlePagination } = require('../modules/handle-pagination');
 
 module.exports = {
    name: 'interactionCreate',
    async execute(interaction) {
       const client = interaction.client;
-      if (!interaction.isCommand() && !interaction.isSelectMenu()) return;
+      if (!interaction.isCommand() && !interaction.isSelectMenu() && !interaction.isMessageComponent()) return;
 
       switch (interaction.customId) {
          case 'podcastselect':
             await stopPlaying();
-            await interaction.update( { content: "Downloading...", components: []});
+            await interaction.update( { content: "Loading...", components: []});
 
             await startPlaying(interaction.member.voice.channel, interaction.values[0]);
             await interaction.deleteReply();
             await interaction.channel.send({ content: "Playing!" });
             break
-         case 'podcast_next':
-            // Update selectmenu with new value components
+         case 'podcastnext':
+            const page = handlePagination();
+            await interaction.update({ content: `Please select a mix or podcast: (Page ${page[0]+1}/${page[1]})`, components: [page[2][0], page[2][1]] });
             break
-      }
-      if (interaction.customId === 'podcastselect') {
+         case 'podcastback':
+            const pageBack = handlePagination('back');
+            await interaction.update({ content: `Please select a mix or podcast: (Page ${pageBack[0]+1}/${pageBack[1]})`, components: [pageBack[2][0], pageBack[2][1]] });
+            break
       }
 
       const command = client.commands.get(interaction.commandName);
